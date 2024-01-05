0c374122a6999926e001d875f00351bc32a3c15e
IWABUCHI Yu(u)ki <butchiyu+com-github@gmail.com>
2024-01-05 16:15:28 +0900

    aboutページ実装

---
+++ pages/about.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { useHead } from "nuxt/app"
+import { useAsyncData } from 'nuxt/app'
+import { ref } from 'vue'
+
+const runtimeConfig = useRuntimeConfig()
+const baseUrl = runtimeConfig.public?.baseUrl
+
+const title = "サイトについて"
+const siteName = "SPW防災サイト"
+const description = "主に防災に関する公式発表へのリンクを掲載する一次情報まとめサイトです。"
+const ogImg = "ogp.png"
+
+useHead({
+    title: title + " | " + siteName,
+    meta: [
+        { property: "title", content: title + " | " + siteName },
+        { property: "description", content: description },
+        { property: "og:type", content: "website" },
+        { property: "og:site_name", content: siteName },
+        { property: "og:image", content: `${baseUrl}${ogImg}` },
+        { property: "og:url", content: baseUrl },
+        { property: "og:title", content: title },
+        { property: "og:description", content: description },
+        { name: "twitter:card", content: "summary_large_image" },
+        { name: "twitter:site", content: "@spw_bousai" },
+        { name: "twitter:creator", content: "@spw_bousai" },
+
+    ],
+})
+</script>
+
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <h2 class="text-h5 mb-3">
+          SPW防災サイトについて
+        </h2>
+
+        <p>このサイトは主に防災に関する公式発表へのリンクを掲載する一次情報まとめサイトです。</p>
+
+        <p>個人の有志によって運営されています。</p>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
